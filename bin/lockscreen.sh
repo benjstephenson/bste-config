dd79edecf81b65633393da02d26a457e4ae05bea
Ben Stephenson <bjames.stephenson@gmail.com>
2016-07-25 18:18:48 +0100

    Move deleted scripts back to bin

--- bin/lockscreen.sh
+++ bin/lockscreen.sh
@@ -3,13 +3,13 @@
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 
-if [[ -f $HOME/.config/i3/scripts/lock.png ]] 
+if [[ -f $HOME/Pictures/lock.png ]]
 then
     # placement x/y
     PX=0
     PY=0
     # lockscreen image info
-    R=$(file ~/.config/i3/scripts/lock.png | grep -o '[0-9]* x [0-9]*')
+    R=$(file ~/Pictures/lock.png | grep -o '[0-9]* x [0-9]*')
     RX=$(echo $R | cut -d' ' -f 1)
     RY=$(echo $R | cut -d' ' -f 3)
 
@@ -24,10 +24,10 @@ then
         PX=$(($SROX + $SRX/2 - $RX/2))
         PY=$(($SROY + $SRY/2 - $RY/2))
 
-        convert /tmp/screen.png $HOME/.config/i3/scripts/lock.png -geometry +$PX+$PY -composite -matte  /tmp/screen.png
+        convert /tmp/screen.png $HOME/Pictures/lock.png -geometry +$PX+$PY -composite -matte  /tmp/screen.png
         echo "done"
     done
-fi 
+fi
 # dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 # i3lock  -I 10 -d -e -u -n -i /tmp/screen.png
 i3lock -e -n -i /tmp/screen.png
